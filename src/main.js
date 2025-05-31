76fb7e03660dbcd6d66c4395b0a37a8aaea4bdb1
melynr <melynsanah@gmail.com>
2025-05-31 15:59:12 +0700

    init repo

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Typed from "typed.js";
+
+const myTyped = new Typed("#greeting", {
+  strings: [
+    "Comp Engineering Student",
+    "Devcsale student",
+    "learning a new skill on Devscale",
+  ],
+  loop: true,
+});
